e34a7c36ba3f854f901d0d1a5391182e5f0b354b
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-06-25 13:39:58 +0300

    odd numbers

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for (int i = 0; i < 10 ; i++) {
+            if (i%2 == 0) continue;
+            System.out.println(i + "");
+
+        }
+    }
+}
